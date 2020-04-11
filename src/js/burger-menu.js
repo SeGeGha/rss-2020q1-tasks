23a0cba8a13c0c4ad2e7b58a9608e04052541c44
Siarhei Sudakov <s3gaddtv@gmail.com>
2020-04-11 22:06:37 +0300

    feat: add html markup and style for category page

--- src/js/burger-menu.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const burgerMenu = document.querySelector('.burger-menu');
-
-  burgerMenu.querySelector('.burger-menu__btn').addEventListener('click', (event) => {
-    event.target.classList.toggle('burger-menu__btn_active');
-  });
-});
