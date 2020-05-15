8619b7cd43bea9dc0226f1cc48a7e9ead302637e
Siarhei Sudakou <s3gaddtv@gmail.com>
2020-05-15 19:58:51 +0300

    init: start fancy-weather task

---
+++ fancy-weather/webpack.config.js
@@ -0,0 +1,71 @@
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'none' : 'source-map',
+    entry: ['./src/js/index.js', './src/css/style.scss'],
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'main.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                outputPath: './assets/img',
+                name: '[name].[ext]',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.html$/,
+          loader: 'html-loader',
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+      }),
+      new CopyPlugin([
+        { from: './src/assets/audio', to: './assets/audio' },
+        { from: './src/assets/img', to: './assets/img' },
+      ]),
+    ],
+  };
+
+  return config;
+};
