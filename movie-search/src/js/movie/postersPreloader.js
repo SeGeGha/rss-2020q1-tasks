9a83f052ec16aaea53e5cb55787fc78af672c45e
Siarhei Sudakou <s3gaddtv@gmail.com>
2020-05-21 18:48:05 +0300

    refactor: change structure of postersPreloader function

--- movie-search/src/js/movie/postersPreloader.js
+++ movie-search/src/js/movie/postersPreloader.js
@@ -1,24 +1,18 @@
-async function uploadMoviesPoster(posters) {
+function uploadMoviesPoster(posters) {
   const imageStub = './assets/img/no-poster.jpg';
 
-  const uploadedPosters = [];
-
-  const promises = posters.map((poster) => new Promise((resolve) => {
+  const uploadedPosters = posters.map((poster) => new Promise((resolve) => {
     const image = new Image();
     image.src = (poster !== 'N/A') ? poster : imageStub;
     image.onload = (event) => {
-      uploadedPosters.push(event.target.src);
-      resolve();
+      resolve(event.target.src);
     };
     image.onerror = () => {
-      uploadedPosters.push(imageStub);
-      resolve();
+      resolve(imageStub);
     };
   }));
 
-  await Promise.all(promises);
-
-  return uploadedPosters;
+  return Promise.all(uploadedPosters);
 }
 
 export default uploadMoviesPoster;
